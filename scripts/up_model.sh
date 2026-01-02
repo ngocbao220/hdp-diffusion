656e91065077d09907a2d30bcfc01ab71cacf48d
ngocbao220 <ngocbaotrinhtuan@gmail.com>
2026-01-02 09:04:24 +0000

    update

---
+++ scripts/up_model.sh
@@ -0,0 +1,4 @@
+huggingface-cli upload \
+ngocbao05/gsm8k-hdp-2-1  \
+outputs/hdp_diffusion/2026.01.02/064546/outputs/hdp_diffusion_h200_bs16/checkpoints \
+--commit-message "Upload GSM8K checkpoint"
